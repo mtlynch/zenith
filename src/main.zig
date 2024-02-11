4933c1b476ff695c588bbb3fbfd10beb97113d07
Michael Lynch <git@mtlynch.io>
2024-02-11 17:05:38 -0500

    work in progress

--- src/main.zig
+++ src/main.zig
@@ -3,14 +3,20 @@ const std = @import("std");
 // Example bytecode we want to execute.
 //
 // // Set the stat
-// PUSH32 0xFF01000000000000000000000000000000000000000000000000000000000000
+// PUSH1 1
 // PUSH1 0
 // MSTORE
 
 // // Example
-// PUSH1 2
-// PUSH1 0
+// PUSH1 1
+// PUSH1 31
 // RETURN
+//
+// Should end with
+// Return value: 0x01
+// Stack: empty
+// Storage: empty
+// Memory: 0x01
 
 const OpCode = enum(u8) {
     PUSH1 = 0x60,
@@ -20,15 +26,42 @@ const OpCode = enum(u8) {
 };
 
 pub fn main() !void {
-    // Dummy bytecode
+    // zig fmt: off
     const bytecode = [_]u8{
-        @intFromEnum(OpCode.PUSH1),
-        @intFromEnum(OpCode.PUSH32),
+        @intFromEnum(OpCode.PUSH1), 0x01,
+        @intFromEnum(OpCode.PUSH1), 0x00,
+        @intFromEnum(OpCode.MSTORE),
+        @intFromEnum(OpCode.PUSH1), 0x01,
+        @intFromEnum(OpCode.PUSH1), 0x1f,
+        @intFromEnum(OpCode.RETURN),
     };
+    // zig fmt: on
 
-    for (bytecode) |b| {
+    var byteIndex: u32 = 0;
+    while (byteIndex < bytecode.len) {
+        const b = bytecode[byteIndex];
+        byteIndex = byteIndex + 1;
+
         const op: OpCode = @enumFromInt(b);
-        std.debug.print("op is {s}\n", .{@tagName(op)});
+        switch (op) {
+            OpCode.PUSH1 => {
+                std.debug.print("Handle {s}\n", .{@tagName(op)});
+                byteIndex = byteIndex + 1;
+                // TODO: Read values onto stack.
+            },
+            OpCode.PUSH32 => {
+                std.debug.print("Handle {s}\n", .{@tagName(op)});
+                // TODO: Read values onto stack.
+            },
+            OpCode.MSTORE => {
+                std.debug.print("Handle {s}\n", .{@tagName(op)});
+                // TODO: Move values from stack to memory
+            },
+            OpCode.RETURN => {
+                std.debug.print("Handle {s}\n", .{@tagName(op)});
+                // TODO: Return value in memory
+            },
+        }
     }
 }
 
