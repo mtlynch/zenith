3d58809c20ec94670bcd8a1e3ef9648b72a81ca0
Michael Lynch <git@mtlynch.io>
2024-02-18 07:14:02 -0500

    Add ZLS to project
    The VS Code setting is a bit of a hack, but I can't figure out any other way to make it work.

--- flake.nix
+++ flake.nix
@@ -4,25 +4,30 @@
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
 
+    zls_dep.url = "github:NixOS/nixpkgs/160b762eda6d139ac10ae081f8f78d640dd523eb";
+
     # 0.11.0
     zig_dep.url = "github:NixOS/nixpkgs/46688f8eb5cd6f1298d873d4d2b9cf245e09e88e";
   };
 
-  outputs = { self, flake-utils, zig_dep }@inputs :
+  outputs = { self, flake-utils, zig_dep, zls_dep }@inputs :
     flake-utils.lib.eachDefaultSystem (system:
     let
       zig_dep = inputs.zig_dep.legacyPackages.${system};
+      zls_dep = inputs.zls_dep.legacyPackages.${system};
     in
     {
       devShells.default = zig_dep.mkShell {
         packages = [
           zig_dep.xxd
           zig_dep.zig
+          zls_dep.zls
         ];
 
         shellHook = ''
           xxd --version 2>&1
-          echo "zig" "$(zig version)"
+          echo 'zls' "$(zls --version)"
+          echo 'zig' "$(zig version)"
         '';
       };
     });
