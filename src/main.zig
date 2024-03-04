bd02a26d794327b9df15814d67bb55663069e508
Michael Lynch <git@mtlynch.io>
2024-03-04 13:48:11 -0500

    work in progress

--- src/main.zig
+++ src/main.zig
@@ -3,9 +3,9 @@ const stack = @import("stack.zig");
 const vm = @import("vm.zig");
 
 pub fn main() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    const allocator = gpa.allocator();
-    defer _ = gpa.deinit();
+    var buffer: [2000]u8 = undefined;
+    var fba = std.heap.FixedBufferAllocator.init(&buffer);
+    const allocator = fba.allocator();
 
     const in = std.io.getStdIn();
     var buf = std.io.bufferedReader(in.reader());
