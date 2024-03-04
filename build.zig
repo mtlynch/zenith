084e9b57ac19e16e566ddcc1a40aaf0aa111b18a
Michael Lynch <git@mtlynch.io>
2024-03-03 20:18:11 -0500

    poc: count bytes

--- build.zig
+++ build.zig
@@ -5,7 +5,7 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
 
     const exe = b.addExecutable(.{
-        .name = "eth-zvm",
+        .name = "count-bytes",
         .root_source_file = .{ .path = "src/main.zig" },
         .target = target,
         .optimize = optimize,
@@ -22,15 +22,4 @@ pub fn build(b: *std.Build) void {
 
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
-
-    const unit_tests = b.addTest(.{
-        .root_source_file = .{ .path = "src/main.zig" },
-        .target = target,
-        .optimize = optimize,
-    });
-
-    const run_unit_tests = b.addRunArtifact(unit_tests);
-
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_unit_tests.step);
 }
--- src/main.zig
+++ src/main.zig
@@ -1,35 +1,30 @@
 const std = @import("std");
-const stack = @import("stack.zig");
-const vm = @import("vm.zig");
+
+pub fn countBytes(reader: anytype) !u32 {
+    var count: u32 = 0;
+    while (true) {
+        _ = reader.readByte() catch |err| switch (err) {
+            error.EndOfStream => {
+                return count;
+            },
+            else => {
+                return err;
+            },
+        };
+        count += 1;
+    }
+}
 
 pub fn main() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    const allocator = gpa.allocator();
-    defer _ = gpa.deinit();
-
     var reader = std.io.getStdIn().reader();
-
-    var evm = vm.VM{};
-    evm.init(allocator);
-    defer evm.deinit();
 
     var timer = try std.time.Timer.start();
     const start = timer.lap();
-    try evm.run(&reader);
+    const count = try countBytes(&reader);
     const end = timer.read();
     const elapsed_micros = @as(f64, @floatFromInt(end - start)) / std.time.ns_per_us;
 
     const output = std.io.getStdOut().writer();
-    try output.print("EVM gas used:    {}\n", .{evm.gasConsumed});
+    try output.print("bytes:           {}\n", .{count});
     try output.print("execution time:  {d:.3}µs\n", .{elapsed_micros});
-    if (evm.returnValue.len > 0) {
-        try output.print("0x{}\n", .{std.fmt.fmtSliceHexLower(evm.returnValue)});
-    } else {
-        // Match evm behavior by outputting a blank line when there is no return value.
-        try output.print("\n", .{});
-    }
-}
-
-test {
-    std.testing.refAllDecls(@This());
 }
