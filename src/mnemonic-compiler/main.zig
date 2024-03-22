44d91927e76037cc4d0c20e2dbd383bd183d87d9
Michael Lynch <git@mtlynch.io>
2024-03-22 06:03:53 -0400

    work in progress

--- src/mnemonic-compiler/main.zig
+++ src/mnemonic-compiler/main.zig
@@ -12,16 +12,19 @@ pub fn main() !void {
     };
     const keywords = std.ComptimeStringMap(vm.OpCode, kvs);
 
+    // TODO: Read input file
+
+    // TODO: Tokenize input file
+
     const output = std.io.getStdOut().writer();
 
     for (keywords.kvs) |kv| {
         try output.print("{s} = 0x{x:0>2}\n", .{ kv.key, @intFromEnum(kv.value) });
     }
 
-    const words = [_][]const u8{ "PUSH1", "0x01", "PUSH1", "0x00", "MSTORE", "PUSH1", "0x20", "PUSH1", "0x00", "RETURN" };
-    for (words) |word| {
-        const val = keywords.get(word) orelse continue;
-        //try output.print("{s} is keyword? {any}", .{ word, keywords.has(word) });
-        try output.print("{s} has value? 0x{x:0>2}\n", .{ word, @intFromEnum(val) });
+    const tokens = [_][]const u8{ "PUSH1", "0x01", "PUSH1", "0x00", "MSTORE", "PUSH1", "0x20", "PUSH1", "0x00", "RETURN" };
+    for (tokens) |token| {
+        const val = keywords.get(token) orelse continue;
+        try output.print("{s} has value? 0x{x:0>2}\n", .{ token, @intFromEnum(val) });
     }
 }
