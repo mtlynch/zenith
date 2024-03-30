e753fc99496c9abae0f498467f34a5f9c2a2c61a
Michael Lynch <mtlynch@users.noreply.github.com>
2024-03-30 09:04:26 -0400

    Make stack debug output two hex digits (#62)

--- src/evm/stack.zig
+++ src/evm/stack.zig
@@ -10,7 +10,7 @@ pub const Stack = struct {
     size: u16 = 0,
 
     pub fn push(self: *Stack, val: u256) !void {
-        std.log.debug("  Stack: push 0x{x}", .{val});
+        std.log.debug("  Stack: push 0x{x:0>2}", .{val});
         if (self.size == self.slots.len) {
             return StackError.Overflow;
         }
@@ -24,7 +24,7 @@ pub const Stack = struct {
         }
         self.size -= 1;
         const val = self.slots[self.size];
-        std.log.debug("  Stack: pop 0x{x}", .{val});
+        std.log.debug("  Stack: pop 0x{x:0>2}", .{val});
         return val;
     }
 
