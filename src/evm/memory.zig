e9ce5abf79bc851e9ca8d81c17c2be42c27641af
Michael Lynch <mtlynch@users.noreply.github.com>
2024-04-07 20:36:39 -0400

    Adjust memory.zig to match the style guide (#78)
    Related: #55
    
    https://ziglang.org/documentation/master/#Style-Guide

--- src/evm/memory.zig
+++ src/evm/memory.zig
@@ -16,35 +16,35 @@ pub const ExpandableMemory = struct {
     }
 
     pub fn write(self: *ExpandableMemory, offset: u256, value: u256) !void {
-        const offsetUsize = std.math.cast(usize, offset) orelse return MemoryError.MemoryReferenceTooLarge;
+        const offset_usize = std.math.cast(usize, offset) orelse return MemoryError.MemoryReferenceTooLarge;
         std.log.debug("  Memory: Writing value=0x{x} to memory offset={d}", .{ value, offset });
-        const wordsToDelete = @min(1, self.storage.items[offsetUsize..].len);
-        try self.storage.replaceRange(offsetUsize, wordsToDelete, &[_]u256{value});
+        const words_to_delete = @min(1, self.storage.items[offset_usize..].len);
+        try self.storage.replaceRange(offset_usize, words_to_delete, &[_]u256{value});
     }
 
     pub fn read(self: ExpandableMemory, allocator: std.mem.Allocator, offset: u256, size: u256) ![]u8 {
-        const offsetUsize = std.math.cast(usize, offset) orelse return MemoryError.MemoryReferenceTooLarge;
-        const sizeUsize = std.math.cast(usize, size) orelse return MemoryError.MemoryReferenceTooLarge;
+        const offset_usize = std.math.cast(usize, offset) orelse return MemoryError.MemoryReferenceTooLarge;
+        const size_usize = std.math.cast(usize, size) orelse return MemoryError.MemoryReferenceTooLarge;
 
-        std.log.debug("  Memory: reading size={d} bytes from offset={d}", .{ sizeUsize, offsetUsize });
+        std.log.debug("  Memory: reading size={d} bytes from offset={d}", .{ size_usize, offset_usize });
 
         // Make a copy of memory in big-endian order.
         // TODO: We can optimize this to only copy the bytes that we want to read.
-        var memoryCopy = try std.ArrayList(u256).initCapacity(allocator, self.storage.items.len);
-        defer memoryCopy.deinit();
+        var memory_copy = try std.ArrayList(u256).initCapacity(allocator, self.storage.items.len);
+        defer memory_copy.deinit();
         for (0..self.storage.items.len) |i| {
-            memoryCopy.insertAssumeCapacity(i, toBigEndian(self.storage.items[i]));
+            memory_copy.insertAssumeCapacity(i, toBigEndian(self.storage.items[i]));
         }
 
-        const mBytes = std.mem.sliceAsBytes(memoryCopy.items);
+        const mem_bytes = std.mem.sliceAsBytes(memory_copy.items);
 
-        var rBytes = try std.ArrayList(u8).initCapacity(allocator, sizeUsize);
-        errdefer rBytes.deinit();
-        for (0..sizeUsize) |i| {
-            try rBytes.insert(i, mBytes[offsetUsize + i]);
+        var r_bytes = try std.ArrayList(u8).initCapacity(allocator, size_usize);
+        errdefer r_bytes.deinit();
+        for (0..size_usize) |i| {
+            try r_bytes.insert(i, mem_bytes[offset_usize + i]);
         }
 
-        return try rBytes.toOwnedSlice();
+        return try r_bytes.toOwnedSlice();
     }
 
     pub fn slice(self: ExpandableMemory) []u256 {
@@ -73,9 +73,9 @@ fn testRead(
     for (memory, 0..) |b, i| {
         try mem.write(i, b);
     }
-    const rBytes = try mem.read(allocator, offset, size);
-    defer allocator.free(rBytes);
-    try std.testing.expectEqualSlices(u8, expected, rBytes);
+    const r_bytes = try mem.read(allocator, offset, size);
+    defer allocator.free(r_bytes);
+    try std.testing.expectEqualSlices(u8, expected, r_bytes);
 }
 
 test "read from memory as bytes" {
@@ -108,11 +108,11 @@ test "overwrite a byte of memory" {
     defer mem.deinit();
     try mem.write(0, 0x01);
     try mem.write(0, 0x02);
-    const rBytes = try mem.read(allocator, 31, 1);
-    defer allocator.free(rBytes);
+    const r_bytes = try mem.read(allocator, 31, 1);
+    defer allocator.free(r_bytes);
 
-    const expectedRead = [_]u8{0x02};
-    try std.testing.expectEqualSlices(u8, &expectedRead, rBytes);
+    const expected_read = [_]u8{0x02};
+    try std.testing.expectEqualSlices(u8, &expected_read, r_bytes);
 }
 
 test "overwrite 32 bytes of memory" {
@@ -122,9 +122,9 @@ test "overwrite 32 bytes of memory" {
     defer mem.deinit();
     try mem.write(0, 0x000000000000000000000000000000000000000000000000000000000000ffff);
     try mem.write(0, 0xffff000000000000000000000000000000000000000000000000000000000000);
-    const rBytes = try mem.read(allocator, 0, 32);
-    defer allocator.free(rBytes);
+    const r_bytes = try mem.read(allocator, 0, 32);
+    defer allocator.free(r_bytes);
 
-    const expectedRead = [_]u8{ 0xff, 0xff, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
-    try std.testing.expectEqualSlices(u8, &expectedRead, rBytes);
+    const expected_read = [_]u8{ 0xff, 0xff, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+    try std.testing.expectEqualSlices(u8, &expected_read, r_bytes);
 }
