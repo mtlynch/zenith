7c7a41bd34bd7b8927dfb39a414fd0ffd685e3a5
Michael Lynch <git@mtlynch.io>
2025-01-21 20:53:21 -0500

    work in progress

--- flake.nix
+++ flake.nix
@@ -19,11 +19,16 @@
           })
         ];
       };
-      zigVersion = "master-2025-01-20";
+      zigVersion = "0.13.0";
       zig = pkgs.zigpkgs.${zigVersion};
       zls = inputs.zls-overlay.packages.${system}.zls.overrideAttrs (old: {
         nativeBuildInputs = [ zig ];
       });
+
+      mkScript = name: text: pkgs.writeShellScriptBin name ''
+        set -eux
+        ${text}
+      '';
     in
     {
       devShells.default = pkgs.mkShell {
@@ -38,6 +43,41 @@
           echo 'zls' "$(zls --version)"
           echo 'zig' "$(zig version)"
         '';
+      };
+
+      apps = {
+        test = flake-utils.lib.mkApp {
+          drv = mkScript "test" ''
+            ${zig}/bin/zig build test --summary all
+          '';
+        };
+
+        build-release-safe = flake-utils.lib.mkApp {
+          drv = mkScript "build-release-safe" ''
+            ${zig}/bin/zig build -Doptimize=ReleaseSafe --verbose
+          '';
+        };
+
+        build-release-fast = flake-utils.lib.mkApp {
+          drv = mkScript "build-release-fast" ''
+            ${zig}/bin/zig build -Doptimize=ReleaseFast --verbose
+          '';
+        };
+
+        compile-testcases = flake-utils.lib.mkApp {
+          drv = mkScript "compile-testcases" ''
+            ${zig}/bin/zig build -Doptimize=ReleaseSafe --verbose
+            ./dev-scripts/compile-testcases
+          '';
+        };
+
+        benchmark = flake-utils.lib.mkApp {
+          drv = mkScript "benchmark" ''
+            ${zig}/bin/zig build -Doptimize=ReleaseFast --verbose
+            ./dev-scripts/compile-testcases
+            dev-scripts/benchmark-all-inputs ./zig-out/bin/zenith 30
+          '';
+        };
       };
     });
 }
